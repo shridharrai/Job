3f44973e375dc1b2d8759da3ac50aadf9b205372
shridharrai <shridharrai3@gmail.com>
2021-08-06 07:06:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from './history';
+import LandingPage from './containers/landingPage';
+import SignUp from './containers/signUp';
+import Login from './containers/login';
+import RecruiterDashboard from './containers/RecruiterDashboard';
+import PostJob from './containers/PostJob';
+import JobCandidates from './containers/JobCandidates';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='ui container'>
+      <Router history={history}>
+        <div>
+          <Switch>
+            <Route path='/' exact component={LandingPage} />
+            <Route path='/signup' exact component={SignUp} />
+            <Route path='/login' exact component={Login} />
+
+            <Route path='/recruiter/dashboard' component={RecruiterDashboard} />
+            <Route path='/post/job' component={PostJob} />
+            <Route path='/job/candidates' component={JobCandidates} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.log(err);
+    return undefined;
+  }
+}
+
+const persistedState = loadFromLocalStorage();
+const store = createStore(reducers, persistedState, applyMiddleware(thunk));
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
